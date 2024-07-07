27f2040967fb417a4cfe9212b530d6f8c7f1ee99
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-08 01:23:27 +0530

    sign in functionality implemented

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const router = require ("express").Router();
 const User = require("../models/user");
-
+const bcryptjs = require("bcryptjs");
 //Sign Up
 router.post("/sign-up",async (req, res) => {
     try {
@@ -30,11 +30,12 @@ router.post("/sign-up",async (req, res) => {
         if(password.length <= 5){
             return res.status(400).json({message: "Password length should be greater than 5"});
         }
-        // console.log("Password length is valid");
+
+        const hashPass = await bcryptjs.hash(password,10);
         const newUser = new User ({
             username:username,
             email:email,
-            password:password,
+            password:hashPass,
             address:address
         });
         await newUser.save();
@@ -44,5 +45,28 @@ router.post("/sign-up",async (req, res) => {
         res.status(500).json({message: "Internal server error"});
     }
 });
+//Sign In
+router.post("/sign-in",async (req, res) => {
+    try {
+       const{username,password} = req.body;
 
+       const exixtingUser = await User.findOne({username});
+       
+       if(!exixtingUser){ //agar user hai hi to invalid direct
+        res.status(400).json({message: "Invalid Credentials"});
+       }
+
+       await bcryptjs.compare(password,exixtingUser.password,(err,data)=>{
+        if(data){ //agar enter kiya hu data hamre database se match kar jata hai to sign in success
+            res.status(200).json({message: "Sign In Successfully"});
+        }
+        else { // hum user ko nhi bata rahe hai ki tumne username ya password galat bhara just telling invalid credentials
+            res.status(400).json({message: "Invalid Credentials"});
+        }
+       })
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({message: "Internal server error"});
+    }
+});
 module.exports = router;
