ad73d373de41a9851abcb7e9c2fcac47567a9220
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-09 16:46:26 +0530

    coding backend: created update and delete book api

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,10 @@ app.use(express.json());
 require("dotenv").config();
 const {conn} = require("./conn/conn")
 const user = require("./routes/user")
-
+const Books = require("./routes/book")
 //routes
 app.use("/api/v1",user);
+app.use("/api/v1",Books);
 conn();
 
 app.listen(process.env.PORT, (error) => {
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -24,5 +24,39 @@ router.post("/add-book",authenticateToken, async (req,res) =>{
     } catch (error) {
         res.status(500).json({message: "Internal server error"});
     }
-})
+});
+//update books
+router.put("update-book",authenticateToken, async (req,res) =>{
+    try {
+        const {Bookid} = req.headers;
+        await Book.findByIdAndUpadate(bookid,{
+            url: req.body.url,
+            title: req.body.title,
+            author: req.body.author,
+            price: req.body.price,
+            desc: req.body.desc,
+            language: req.body.language,
+        });
+        return res.status(200).json({
+            message: "Book Update Successfully!",
+        });
+    } catch (error) {
+        // console.log(error);
+        return res.status(500).json({message: "An error occured"});
+    }
+});
+//delete
+router.delete("/delete-book", authenticateToken, async (req,res)=> {
+try {
+    const {bookid} = req.headers;
+    await Book.findByIdAndDelete(bookid);
+    return res.status(200).json({
+        message: "Book deleted successfully!",
+    });
+} catch (error) {
+    // console.log(error);
+        return res.status(500).json({message: "An error occured"});
+}
+});
+//get all books api
 module.exports = router;
