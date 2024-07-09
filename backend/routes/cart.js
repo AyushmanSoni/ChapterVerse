dc44cd7a7e51532de4c7304078ba9160d4b33560
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-09 20:01:11 +0530

    coding backend: created get user cart api

--- backend/routes/cart.js
+++ backend/routes/cart.js
@@ -43,4 +43,19 @@ router.put("/remove-from-cart/:bookid", authenticateToken, async (req,res)=> {
         return res.status(500).json({message: "An error occurred"});
     }
 });
+//get cart of perticular user
+router.get("/get-user-cart", authenticateToken, async (req,res)=> {
+    try {
+        const {id} = req.headers;
+        const userData = await User.findById(id).populate("cart");
+        const cart = userData.cart.reverse();
+        return res.json({
+            status: "Success",
+            data: cart,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "An error occured"});
+    }
+})
 module.exports = router;
