0677017c52dd6394ff8ed1238ec95575c75906b7
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-10 02:13:52 +0530

    coding backend: created place order api

--- backend/app.js
+++ backend/app.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
-const {conn} = require("./conn/conn")
-const user = require("./routes/user")
-const Books = require("./routes/book")
-const favourite = require("./routes/favourite")
-const Cart = require("./routes/cart")
+const {conn} = require("./conn/conn");
+const user = require("./routes/user");
+const Books = require("./routes/book");
+const favourite = require("./routes/favourite");
+const Cart = require("./routes/cart");
+const Order = require("./routes/order");
 //routes
 app.use("/api/v1",user);
 app.use("/api/v1",Books);
 app.use("/api/v1",favourite);
 app.use("/api/v1",Cart);
+app.use("/api/v1",Order);
 conn();
 
 app.listen(process.env.PORT, (error) => {
---
+++ backend/routes/order.js
@@ -0,0 +1,33 @@
+const router = require ("express").Router();
+const {authenticateToken} = require("./userAuth");
+const Book = require("../models/book");
+const Order = require("../models/order");
+const User = require("../models/user");
+
+//place order
+router.post("/place-order", authenticateToken, async (req,res)=>{
+    try {
+        const {id} = req.headers;
+        const {order} = req.body;
+        for(const orderData of order){
+            const newOrder = new Order({user: id,book: orderData._id});
+            const orderDataFromDb = await newOrder.save();
+            //saving order in user model
+            await User.findbyIdAndUpdate(id,{
+                $push: {orders: orderDataFromDb._id},
+            });
+            //clearing cart
+            await User.findByIdAndUpdate(id,{
+                $pull: {cart: orderData._id},
+            });
+        }
+        return res.json({
+            status: "Success",
+            message: "Order Placed Successflly",
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "An error occured"});
+    }
+})
+module.exports = router;
