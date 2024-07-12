5a49e3c6915ca95d7e9e68f80be503683a661d15
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-12 19:24:37 +0530

    coding frontend: add hero text and button

--- frontend/src/components/Home/Hero.jsx
+++ frontend/src/components/Home/Hero.jsx
@@ -3,8 +3,37 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='h-[75vh] '>
-        <div className='w-3/6'> <h1>Discover your Next Great Read</h1></div>
-        <div className='w-3/6'></div>
+        <div className='w-3/6 py-4 mt-12'> <h1 className='text-6xl font-bold text-[#032B37]'>Today A&nbsp; 
+        <span className='text-[#086D8A]'>Reader,</span></h1></div>
+        <div className='w-3/6'> <h1 className='text-6xl font-bold text-[#032B37]'>Tomorrow A&nbsp; 
+        <span className='text-[#086D8A]'>Leader</span></h1></div>
+        <div className='w-3/6'>
+            <p className='text-xl text-gray-500 mt-8 font-medium'>
+                Find your next great read from our extensive collection of books across all genres. Join our community of book lovers and start your reading journey today!
+            </p></div>
+        <div className='w-3/6 mt-8'>
+            <button className='bg-[#086D8A] text-white text-lg font-semibold py-2 px-8 rounded hover:bg-[#065b70]'>
+                Discover Books
+            </button>
+      </div>
+      <div className='w-3/6 mt-8 grid grid-cols-2 gap-4'>
+        <div className='flex items-center'>
+          <span className='text-green-500 mr-2'>✔️</span>
+          <span className='text-gray-500 font-medium'>Wide range of genres</span>
+        </div>
+        <div className='flex items-center'>
+          <span className='text-green-500 '>✔️</span>
+          <span className='text-gray-500 font-medium'>Affordable prices</span>
+        </div>
+        <div className='flex items-center'>
+          <span className='text-green-500 mr-2'>✔️</span>
+          <span className='text-gray-500 font-medium'>Exclusive collections</span>
+        </div>
+        <div className='flex items-center'>
+          <span className='text-green-500 mr-2'>✔️</span>
+          <span className='text-gray-500 font-medium'>Fast shipping</span>
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -19,9 +19,11 @@ const Navbar = () => {
     ];
   return (
     //justify between se links aur chapterverse opposite side chale jate h
-    <div className='flex bg-[#F3F8F9] text-grey px-24 py-4 itmes-center justify-between'> 
+    <div className='flex bg-[#F3F8F9] text-grey px-24 py-4 itmes-center 
+    justify-between'> 
         <div className='flex items-center '>
-            <img className='h-10 me-4' src="https://www.svgrepo.com/show/53217/book-and-glasses.svg" alt="logo" />
+            <img className='h-10 me-4' 
+            src="https://www.svgrepo.com/show/53217/book-and-glasses.svg" alt="logo" />
             <h1 className='text-2xl font-semibold text-[#086D8A]'>
                 Chapter<span className='text-[#032B37]'>Verse</span>
             </h1>
@@ -34,8 +36,10 @@ const Navbar = () => {
             )}
         </div>
         <div className='flex gap-8'>
-        <button className='px-4 py-2 text-[#086D8A] hover:text-[#075A71]'>SignIn</button>
-        <button className='px-4 py-2v bg-[#086D8A] text-white border rounded-lg hover:bg-[#075A71]'>SignUp</button>
+        <button className='px-4 py-2 text-[#086D8A] hover:text-[#075A71]'>
+            SignIn</button>
+        <button className='px-4 py-2v bg-[#086D8A] text-white border rounded-lg
+         hover:bg-[#075A71]'>SignUp</button>
         </div>
     </div>
 
