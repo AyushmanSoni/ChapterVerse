72962cd476948e32af1debac40aeaf83922a1602
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-12 16:44:06 +0530

    coding forntend: added logo heading to navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    //justify between se links aur chapterverse opposite side chale jate h
+    <div className='flex bg-[#F3F8F9] text-grey px-8 py-4 itmes-center justify-between'> 
+        <div className='flex items-center '>
+            <img className='h-10 me-4' src="https://www.svgrepo.com/show/53217/book-and-glasses.svg" alt="logo" />
+            <h1 className='text-2xl font-semibold'>ChapterVerse</h1>
+        </div>
+        <div>Links</div>
+    </div>
+
   )
 }
 
