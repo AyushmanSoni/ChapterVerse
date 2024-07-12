ea149c8c7423e7514169d2a24bbe0b937c41b4ce
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-12 17:20:39 +0530

    coding frontend: navbar created

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -19,12 +19,12 @@ const Navbar = () => {
     ];
   return (
     //justify between se links aur chapterverse opposite side chale jate h
-    <div className='flex bg-[#F3F8F9] text-grey px-8 py-4 itmes-center justify-between'> 
+    <div className='flex bg-[#F3F8F9] text-grey px-24 py-4 itmes-center justify-between'> 
         <div className='flex items-center '>
             <img className='h-10 me-4' src="https://www.svgrepo.com/show/53217/book-and-glasses.svg" alt="logo" />
             <h1 className='text-2xl font-semibold'>ChapterVerse</h1>
         </div>
-        <div className='nav-links-chapterverse flex gap-8'>
+        <div className='nav-links-chapterverse flex gap-16 pt-2'>
             {links.map((items,i)=> 
             <div key={i}>
                 {items.title}
@@ -32,12 +32,12 @@ const Navbar = () => {
             )}
         </div>
         <div className='flex gap-8'>
-        <button className='px-4 py-2 border-blue-500'></button>
-        <button className='px-4 py-2v bg-white text-zinc-500'></button>
+        <button className='px-4 py-2 text-[#086D8A] hover:text-[#075A71]'>SignIn</button>
+        <button className='px-4 py-2v bg-[#086D8A] text-white border rounded-lg hover:bg-[#075A71]'>SignUp</button>
         </div>
     </div>
 
-  )
+  ) 
 }
 
 export default Navbar
