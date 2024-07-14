6ceea8b2070e65b8fef0dbec710d0a4bd4164678
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-14 12:41:57 +0530

    fetched book cover images from data to frontend

--- frontend/src/components/Bookcard/Bookcard.jsx
+++ frontend/src/components/Bookcard/Bookcard.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import {Link} from "react-router-dom";
 const Bookcard = ({data}) => {
   console.log(data);
   return (
-    <div>Bookcard</div>
+    <>
+    <Link>
+    <div className='bg-zinc-800 rounded p-4'>
+      <div className='bg-zinc-900'>
+        <img src={data.url} alt="/" />
+      </div>
+    </div>
+    </Link>
+    </>
   )
 }
 
