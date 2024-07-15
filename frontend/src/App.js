7472b01d260826768d924c3bce4ca6e6a2680d70
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-15 18:58:22 +0530

    created auth and reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Home from './pages/Home'
 import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import AllBooks from './pages/AllBooks';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -12,7 +12,7 @@ import Bookdetails from './components/Bookdetails/Bookdetails';
 const App = () => {
   return (
     <div>
-      <Router>
+
         <Navbar/>
         <Routes>
           <Route exact path="/" element={<Home/>}/>
@@ -24,7 +24,7 @@ const App = () => {
           <Route path="/book-details/:id" element= {<Bookdetails/>} />
         </Routes>
         <Footer/>
-      </Router>
+
     </div>
   )
 }
