50208c97b4a359d9469e7e251c7572d9a7f46dcc
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-17 12:47:53 +0530

    created profile page and sidebar in it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,48 @@
-import React, { useEffect } from 'react'
-import Home from './pages/Home'
-import Navbar from './components/Navbar/Navbar'
-import Footer from './components/Footer/Footer'
-import {Routes, Route} from "react-router-dom";
+import React, { useEffect } from 'react';
+import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import { Routes, Route } from "react-router-dom";
 import AllBooks from './pages/AllBooks';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Cart from './pages/Cart';
 import Profile from './pages/Profile';
 import Bookdetails from './components/Bookdetails/Bookdetails';
+import Favourites from './components/Profile/Favourites'; // Make sure this import is correct
 import { useDispatch, useSelector } from 'react-redux';
 import { authActions } from './store/auth';
+
 const App = () => {
   const dispatch = useDispatch();
   const role = useSelector((state) => state.auth.role);
-  useEffect(()=>{
-    if(localStorage.getItem("id") &&
-        localStorage.getItem("token") && 
-        localStorage.getItem("role")
-      ){
-        dispatch(authActions.login());
-        dispatch(authActions.changeRole(localStorage.getItem("role")));
-      }
-  },[]);
+
+  useEffect(() => {
+    if (localStorage.getItem("id") &&
+        localStorage.getItem("token") &&
+        localStorage.getItem("role")) {
+      dispatch(authActions.login());
+      dispatch(authActions.changeRole(localStorage.getItem("role")));
+    }
+  }, [dispatch]);
+
   return (
     <div>
-      
-      <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route path="/all-books" element={<AllBooks/>}/>
-          <Route path="/LogIn" element={<Login/>}/>
-          <Route path="/SignUp" element={<SignUp/>}/>
-          <Route path="/cart" element={<Cart/>}/>
-          <Route path="/profile" element={<Profile/>}/>
-          <Route path="/book-details/:id" element= {<Bookdetails/>} />
-        </Routes>
-        <Footer/>
-        
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/all-books" element={<AllBooks />} />
+        <Route path="/LogIn" element={<Login />} />
+        <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/profile" element={<Profile />}>
+          <Route index element={<Favourites />} />
+        </Route>
+        <Route path="/book-details/:id" element={<Bookdetails />} />
+      </Routes>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Sidebar from "../components/Profile/Sidebar";
+import {Outlet} from "react-router-dom";
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='bg-zinc-900 px-2 md:px-12 flex flex-col md:flex-row h-screen py-8 gap-4'>
+      <div><Sidebar/></div>
+      <div><Outlet/></div>
+    </div>
   )
 }
 
