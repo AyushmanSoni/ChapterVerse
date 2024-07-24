f2a778959c3ad4d6ac6bd131b7a2ae78deab9c33
Ayushman Soni <ayushmanlsoni3660@gmail.com>
2024-07-25 00:38:41 +0530

    temporary last commit

--- frontend/src/components/Profile/OrderHistory.jsx
+++ frontend/src/components/Profile/OrderHistory.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import Loader from '../Loader/Loader';
 
 const OrderHistory = () => {
   const [orderHistory, setOrderHistory] = useState([]);
@@ -26,26 +27,38 @@ const OrderHistory = () => {
     fetchOrderHistory();
   }, []);
 
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p className="text-red-500">{error}</p>;
+  }
+
+  if (orderHistory.length === 0) {
+    return (
+      <div className='h-[80vh] p-4 text-zinc-800'>
+        <div className='h-[100%] flex flex-col items-center justify-center'>
+          <h1 className='text-5xl font-semibold text-zinc-800 mb-8'>No Order History</h1>
+          <img src="https://img.freepik.com/free-vector/empty-concept-illustration_114360-1233.jpg?t=st=1721847516~exp=1721851116~hmac=f2ac5097fe70413ffbb36ed6fb0c2d6766902a2d0bdf6891694b29b052b0bd6d&w=740" alt="" className='h-[20vh] mb-8' />
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="p-4 bg-[#F3F8F9] min-h-screen">
       <h1 className="text-3xl font-semibold text-[#086D8A] mb-4">Order History</h1>
-      {isLoading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-      {!isLoading && !error && orderHistory.length === 0 && (
-        <p className="text-zinc-400">You have no orders yet.</p>
-      )}
-      {!isLoading && orderHistory.length > 0 && (
-        <div className="space-y-4">
-          {orderHistory.map((order, index) => (
-            <div key={index} className="p-4 bg-white rounded border border-[#086D8A]">
-              <h2 className="text-xl font-semibold text-[#086D8A]">{order.book.title}</h2>
-              <p className="text-zinc-700">{order.book.desc}</p>
-              <p className="text-zinc-700">Order Date: {new Date(order.createdAt).toLocaleDateString()}</p>
-              <p className="text-zinc-700">Status: {order.status}</p>
-            </div>
-          ))}
-        </div>
-      )}
+      <div className="space-y-4">
+        {orderHistory.map((order, index) => (
+          <div key={index} className="p-4 bg-white rounded border border-[#086D8A]">
+            <h2 className="text-xl font-semibold text-[#086D8A]">{order.book.title}</h2>
+            <p className="text-zinc-700">{order.book.desc}</p>
+            <p className="text-zinc-700">Order Date: {new Date(order.createdAt).toLocaleDateString()}</p>
+            <p className="text-zinc-700">Status: {order.status}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
